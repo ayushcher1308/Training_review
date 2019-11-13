2371b48c573350ffba0866e7927de2f774087871
814088 <814088@cognizant.com>
2019-11-13 16:09:41 +0530

    csv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,45 +10,90 @@ export class AppComponent implements OnInit {
   modelData;
   ngOnInit() {
     this.modelData = [{
-  "id": "1",
-  "model_name": "model-1",
-  "status": "trained"
-},
-{
-  "id": "2",
-  "model_name": "model-2",
-  "status": "trained"
-},
-{
-  "id": "3",
-  "model_name": "model-3",
-  "status": "trained"
-},
-{
-  "id": "4",
-  "model_name": "model-4",
-  "status": "trained"
-},
-{
-  "id": "5",
-  "model_name": "model-5",
-  "status": "untrained"
-},
-{
-  "id": "6",
-  "model_name": "model-6",
-  "status": "untrained"
-},
-{
-  "id": "7",
-  "model_name": "model-7",
-  "status": "trained"
-},
-{
-  "id": "8",
-  "model_name": "model-8",
-  "status": "untrained"
-}
-];
+      "id": "1",
+      "model_name": "model-1",
+      "status": "trained"
+    },
+    {
+      "id": "2",
+      "model_name": "model-2",
+      "status": "trained"
+    },
+    {
+      "id": "3",
+      "model_name": "model-3",
+      "status": "trained"
+    },
+    {
+      "id": "4",
+      "model_name": "model-4",
+      "status": "trained"
+    },
+    {
+      "id": "5",
+      "model_name": "model-5",
+      "status": "untrained"
+    },
+    {
+      "id": "6",
+      "model_name": "model-6",
+      "status": "untrained"
+    },
+    {
+      "id": "7",
+      "model_name": "model-7",
+      "status": "trained"
+    },
+    {
+      "id": "8",
+      "model_name": "model-8",
+      "status": "untrained"
+    }
+    ];
   }
+
+  file: any;
+  fileReaded;
+  fileChanged(e) {
+    console.log(e);
+    // this.file = e.target.files[0];
+    this.fileReaded = e.target.files[0];
+
+    let reader: FileReader = new FileReader();
+    reader.readAsText(this.fileReaded);
+
+    reader.onload = (e) => {
+      let csv: string = reader.result;
+      let allTextLines = csv.split(/\r|\n|\r/);
+      let headers = allTextLines[0].split(',');
+      let lines = [];
+
+      for (let i = 0; i < allTextLines.length; i++) {
+        // split content based on comma  
+        let data = allTextLines[i].split(',');
+        if (data.length === headers.length) {
+          let tarr = [];
+          for (let j = 0; j < headers.length; j++) {
+            tarr.push(data[j]);
+          }
+
+          // log each row to see output  
+          console.log(tarr);
+          lines.push(tarr);
+        }
+      }
+      // all rows in the csv file  
+      console.log(">>>>>>>>>>>>>>>>>", lines);
+    }
+
+  }
+uploadDocument(file) {
+  console.log("submitted");
+    // let fileReader = new FileReader();
+    // fileReader.onload = (e) => {
+    //   console.log(fileReader.result);
+    // }
+    // fileReader.readAsText(this.file);
+    // return true;
+}
 }
